f83da0d367729325487773fbabb85734c708bf10
Clara Meira <claraa.meiraa@hotmail.com>
2024-04-06 11:38:26 -0300

    Implementando a classe Carro

--- demo/src/main/java/com/example/Carro.java
+++ demo/src/main/java/com/example/Carro.java
@@ -1,4 +1,94 @@
 package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
 public class Carro {
     
+    private String placa;
+    private String cor;
+    private int numPortas;
+    private int quilometragem;
+    private String chassi;
+    private double valorLoc;
+    private String modelo;
+    private boolean disponivel;
+
+    public Carro(String placa, String cor, int numPortas, int quilometragem, String chassi, double valorLoc, String modelo) {
+        this.placa = placa;
+        this.cor = cor;
+        this.numPortas = numPortas;
+        this.quilometragem = quilometragem;
+        this.chassi = chassi;
+        this.valorLoc = valorLoc;
+        this.modelo = modelo;
+        this.disponivel = true; 
+    }
+
+    public void visuCar() {
+        System.out.println("Placa: " + placa);
+        System.out.println("Cor: " + cor);
+        System.out.println("Número de Portas: " + numPortas);
+        System.out.println("Quilometragem: " + quilometragem);
+        System.out.println("Chassi: " + chassi);
+        System.out.println("Valor de Locação: " + valorLoc);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Disponível: " + (disponivel ? "Sim" : "Não"));
+    }
+
+    public void cadastrarCar(List<Carro> listaCarros, Carro novoCarro) {
+        listaCarros.add(novoCarro);
+        System.out.println("Carro cadastrado com sucesso!");
+    }
+
+    public void editarCar(Carro carro, String novaPlaca, String novaCor, int novoNumPortas, int novaQuilometragem, String novoChassi, double novoValorLoc, String novoModelo) {
+        carro.placa = novaPlaca;
+        carro.cor = novaCor;
+        carro.numPortas = novoNumPortas;
+        carro.quilometragem = novaQuilometragem;
+        carro.chassi = novoChassi;
+        carro.valorLoc = novoValorLoc;
+        carro.modelo = novoModelo;
+        System.out.println("Informações do carro atualizadas com sucesso!");
+    }
+
+    public void removerCar(List<Carro> listaCarros, Carro carro) {
+        listaCarros.remove(carro);
+        System.out.println("Carro removido com sucesso!");
+    }
+
+    public void listarCar(List<Carro> listaCarros) {
+        if (listaCarros.isEmpty()) {
+            System.out.println("Não há carros cadastrados.");
+        } else {
+            for (Carro carro : listaCarros) {
+                carro.visuCar();
+                System.out.println("------------------------");
+            }
+        }
+    }
+
+    public void listarCarDisp(List<Carro> listaCarros) {
+        List<Carro> carrosDisponiveis = new ArrayList<>();
+        for (Carro carro : listaCarros) {
+            if (carro.disponivel) {
+                carrosDisponiveis.add(carro);
+            }
+        }
+        if (carrosDisponiveis.isEmpty()) {
+            System.out.println("Não há carros disponíveis.");
+        } else {
+            for (Carro carro : carrosDisponiveis) {
+                carro.visuCar();
+                System.out.println("------------------------");
+            }
+        }
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
