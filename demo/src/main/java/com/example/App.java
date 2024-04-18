85a624e014e97f13cb69fd31e52338241b439711
Clara Meira <claraa.meiraa@hotmail.com>
2024-04-18 08:53:04 -0300

    Iniciando implementação da classe executável

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,7 +1,22 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----------------------------------");
+        System.out.println("Deseja acessar como administrador?");
+        System.out.println("1. Sim");
+        System.out.println("2. Não");
+        System.out.println("----------------------------------");
+        int res = sc.nextInt();
+
+        boolean ehAdmin = (res == 1);
+        System.out.println("Acesso Permitido!");
+
+        sc.close();
     }
+
 }
